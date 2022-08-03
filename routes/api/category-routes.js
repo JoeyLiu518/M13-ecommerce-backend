4c8d51beadf6d41c8115ee71bc1cf68074fbee21
Wazzaubi <joeyliu7886@gmail.com>
2022-08-02 21:46:13 -0700

    changes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,56 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then(data=>{
+    res.json(data)
+  }).catch(err=>{
+    res.status(500).json({msg:"Unable to find Categories",err})
+})
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id,{
+    include:[{
+      model:Product
+    }]
+  }).then(data=>{
+    res.json(data)
+  }).catch(err=>{
+    res.status(500).json({msg:"Unable to find Category",err})
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    categoryName:req.body.categoryName
+  }).then(data=>{
+    res.json(data)
+  }).catch(err=>{
+    res.status(500).json({msg:"Error no category could be created",err})
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name,
+  },{
+    where: {id:req.params.id}
+  }).then(resp=>{
+    res.json(resp);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {id:req.params.id}
+  }).then(resp=>{
+    res.json(resp)
+  })
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,58 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll().then(data=>{
+    res.json(data)
+  }).catch(err=>{
+    res.status(500).json({msg: "Error", err})
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try { 
+    const tagData = await Tag.findByPk(req.params.id);
+  if (!tagData) {
+    res.status(404).json({msg: "Unable to find product"});
+    return;
+  }
+  res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body).then(tag => {
+    res.json(tag);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({ tag_name: req.body.tag_name }, {
+    where: { id: req.params.id }
+  }).then(tag => {
+    res.json(tag);
+  });
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where:{
+      id:req.params.id
+    }
+  }).then(tag=>{
+    if(!tag){
+      return res.status(404).json({msg:"No tag"})
+    }
+    res.json(tag)
+  }).catch(err=>{
+    res.status(500).json({msg:"Error",err})
+  })
 });
 
 module.exports = router;
